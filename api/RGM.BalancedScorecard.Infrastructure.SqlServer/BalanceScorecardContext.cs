525035a1ec5536d2e6427df0ae6da3d7dca21302
Roberto Garcia <robertogarcia@rhs.net>
2016-09-15 16:52:16 +0100

    Added a new project for SQL Server

---
+++ api/RGM.BalancedScorecard.Infrastructure.SqlServer/BalanceScorecardContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using RGM.BalancedScorecard.Domain.Model.Indicators;
+
+namespace RGM.BalancedScorecard.Infrastructure.SqlServer
+{
+    public class BalanceScorecardContext : DbContext
+    {
+        public DbSet<Indicator> Indicators { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Indicator>()
+               .ToTable("Indicators");
+
+            modelBuilder.Entity<Indicator>().Property(i => i.Id).IsRequired();
+            modelBuilder.Entity<Indicator>().Property(i => i.Code).IsRequired();
+            modelBuilder.Entity<Indicator>().Property(i => i.Code).IsRequired();
+            modelBuilder.Entity<Indicator>().Property(i => i.Name).IsRequired();
+            modelBuilder.Entity<Indicator>().Property(i => i.Description).IsRequired();
+            modelBuilder.Entity<Indicator>().Property(i => i.Unit).IsRequired();
+            modelBuilder.Entity<Indicator>().Ignore(i => i.Events);
+        }
+    }
+}
